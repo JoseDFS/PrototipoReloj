0fce2e8de7800ed9e378be65bd0f5f6edb1de337
Jose Segura <00001617@uca.edu.sv>
2022-11-18 15:19:37 -0800

    Quitando metodos inutiles

--- lib/helpers/shared_prefs.dart
+++ lib/helpers/shared_prefs.dart
@@ -8,22 +8,3 @@ LatLng getCurrentLatLngFromSharedPrefs() {
       sharedPreferences.getDouble('longitude')!);
 }
 
-String getCurrentAddressFromSharedPrefs() {
-  return sharedPreferences.getString('current-address')!;
-}
-
-LatLng getTripLatLngFromSharedPrefs(String type) {
-  List sourceLocationList =
-      json.decode(sharedPreferences.getString('source')!)['location'];
-  List destinationLocationList =
-      json.decode(sharedPreferences.getString('destination')!)['location'];
-  LatLng source = LatLng(sourceLocationList[0], sourceLocationList[1]);
-  LatLng destination =
-      LatLng(destinationLocationList[0], destinationLocationList[1]);
-
-  if (type == 'source') {
-    return source;
-  } else {
-    return destination;
-  }
-}
